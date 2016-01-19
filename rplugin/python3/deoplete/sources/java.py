12974805b983b7f008b1ddfbddc317d34c15264f
wsdjeg <wsdjeg@outlook.com>
2016-01-20 00:07:24 +0800

    Implement deoplete source

---
+++ rplugin/python3/deoplete/sources/java.py
@@ -0,0 +1,22 @@
+import deoplete.util
+
+from .base import Base
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'java'
+        self.mark = '[JC]'
+        self.filetypes = ['java']
+        self.min_pattern_length = 0
+        self.is_bytepos = True
+
+    def get_complete_api(self, findstart):
+        return self.vim.call('javacomplete#Complete', findstart, 0)
+
+    def get_complete_position(self, context):
+        return self.get_complete_api(1)
+
+    def gather_candidates(self, context):
+        return self.get_complete_api(0)
