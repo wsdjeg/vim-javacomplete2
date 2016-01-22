5ea01d260635ba0488214db1341488f3181bd3f2
wsdjeg <wsdjeg@outlook.com>
2016-01-22 19:20:02 +0800

    Test java.py
    https://github.com/artur-shaik/vim-javacomplete2/issues/163

--- rplugin/python3/deoplete/sources/java.py
+++ rplugin/python3/deoplete/sources/java.py
@@ -1,22 +1,17 @@
-import deoplete.util
-
 from .base import Base
 
 class Source(Base):
     def __init__(self, vim):
         Base.__init__(self, vim)
 
-        self.name = 'java'
-        self.mark = ''
-        self.filetypes = ['java']
-        self.min_pattern_length = 0
+        self.name = 'javacomplete2'
+        self.mark = '[jc]'
+        self.filetypes = ['java', 'jsp']
         self.is_bytepos = True
-
-    def get_complete_api(self, findstart):
-        return self.vim.call('javacomplete#Complete', findstart, 0)
+        self.min_pattern_length = 0
 
     def get_complete_position(self, context):
-        return self.get_complete_api(1)
+        return self.vim.call('javacomplete#Complete', 1, 0)
 
     def gather_candidates(self, context):
-        return self.get_complete_api(0)
+        return self.vim.call('javacomplete#Complete', 0, context['complete_str'])
